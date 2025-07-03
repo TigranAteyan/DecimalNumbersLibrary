588eed63100b88160c223096585fd1f19a39d4cc
Tigran <tigranateyan2009@gmail.com>
2025-07-03 23:22:03 +0400

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -9,53 +9,42 @@ TEST(FloatPointTests, CanCreateFromString) {
     EXPECT_FLOAT_EQ(a.ToFloat(), 1.25f);
 }
 
-TEST(FloatPointTests, AdditionWorks) {
+TEST(FloatPointTests, CanAdd) {
     FloatPoint a("1.5");
     FloatPoint b("2.5");
     DecimalNumber* result = a + b;
-    auto* res = dynamic_cast<FloatPoint*>(result);
-    ASSERT_NE(res, nullptr);
-    EXPECT_FLOAT_EQ(res->ToFloat(), 4.0f);
+    EXPECT_FLOAT_EQ(result->ToFloat(), 4.0f);
     delete result;
 }
 
-TEST(FloatPointTests, SubtractionWorks) {
+TEST(FloatPointTests, CanSubtract) {
     FloatPoint a("5.5");
     FloatPoint b("2.0");
     DecimalNumber* result = a - b;
-    auto* res = dynamic_cast<FloatPoint*>(result);
-    ASSERT_NE(res, nullptr);
-    EXPECT_FLOAT_EQ(res->ToFloat(), 3.5f);
+    EXPECT_FLOAT_EQ(result->ToFloat(), 3.5f);
     delete result;
 }
 
-TEST(FloatPointTests, MultiplicationWorks) {
+TEST(FloatPointTests, CanMultiply) {
     FloatPoint a("2.0");
     FloatPoint b("3.5");
     DecimalNumber* result = a * b;
-    auto* res = dynamic_cast<FloatPoint*>(result);
-    ASSERT_NE(res, nullptr);
-    EXPECT_FLOAT_EQ(res->ToFloat(), 7.0f);
+    EXPECT_FLOAT_EQ(result->ToFloat(), 7.0f);
     delete result;
 }
 
-TEST(FloatPointTests, DivisionWorks) {
+TEST(FloatPointTests, CanDivide) {
     FloatPoint a("10.0");
     FloatPoint b("2.0");
     DecimalNumber* result = a / b;
-    auto* res = dynamic_cast<FloatPoint*>(result);
-    ASSERT_NE(res, nullptr);
-    EXPECT_FLOAT_EQ(res->ToFloat(), 5.0f);
+    EXPECT_FLOAT_EQ(result->ToFloat(), 5.0f);
     delete result;
 }
 
 TEST(FloatPointTests, DivisionByZeroThrows) {
     FloatPoint a("10.0");
     FloatPoint b("0.0");
-    EXPECT_THROW({
-        DecimalNumber* result = a / b;
-        delete result;
-    }, DivisionByZero);
+    EXPECT_THROW(a / b, DivisionByZero);
 }
 
 TEST(FixedPointTests, CanCreateFromFloat) {
@@ -67,9 +56,7 @@ TEST(FixedPointTests, AdditionWorks) {
     FixedPoint<3,4> a(1.5f);
     FixedPoint<3,4> b(2.25f);
     DecimalNumber* result = a + b;
-    auto* res = dynamic_cast<FixedPoint<3,4>*>(result);
-    ASSERT_NE(res, nullptr);
-    EXPECT_NEAR(res->ToFloat(), 3.75f, 0.001f);
+    EXPECT_NEAR(result->ToFloat(), 3.75f, 0.001f);
     delete result;
 }
 
@@ -77,9 +64,7 @@ TEST(FixedPointTests, SubtractionWorks) {
     FixedPoint<3,4> a(5.0f);
     FixedPoint<3,4> b(1.75f);
     DecimalNumber* result = a - b;
-    auto* res = dynamic_cast<FixedPoint<3,4>*>(result);
-    ASSERT_NE(res, nullptr);
-    EXPECT_NEAR(res->ToFloat(), 3.25f, 0.001f);
+    EXPECT_NEAR(result->ToFloat(), 3.25f, 0.001f);
     delete result;
 }
 
@@ -87,9 +72,7 @@ TEST(FixedPointTests, MultiplicationWorks) {
     FixedPoint<3,4> a(2.0f);
     FixedPoint<3,4> b(3.0f);
     DecimalNumber* result = a * b;
-    auto* res = dynamic_cast<FixedPoint<3,4>*>(result);
-    ASSERT_NE(res, nullptr);
-    EXPECT_NEAR(res->ToFloat(), 6.0f, 0.001f);
+    EXPECT_NEAR(result->ToFloat(), 6.0f, 0.001f);
     delete result;
 }
 
@@ -97,9 +80,7 @@ TEST(FixedPointTests, DivisionWorks) {
     FixedPoint<3,4> a(6.0f);
     FixedPoint<3,4> b(2.0f);
     DecimalNumber* result = a / b;
-    auto* res = dynamic_cast<FixedPoint<3,4>*>(result);
-    ASSERT_NE(res, nullptr);
-    EXPECT_NEAR(res->ToFloat(), 3.0f, 0.001f);
+    EXPECT_NEAR(result->ToFloat(), 3.0f, 0.001f);
     delete result;
 }
 
